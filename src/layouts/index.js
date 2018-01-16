6098a8f334bee2bc1d22bd4ef09daca2df9372e0
sawyerh <s.hollenshead@gmail.com>
2018-01-16 18:27:17 -0500

    Fix path checking when prefix is present

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,30 +4,30 @@ import Helmet from "react-helmet";
 import PropTypes from "prop-types";
 import React from "react";
 import { SkipNav } from "@cmsgov/design-system-core";
+import { withPrefix } from "gatsby-link";
 
-const TemplateWrapper = ({ children, data, location }) => (
-  <div className="ds-base">
-    <SkipNav href="#main" />
-    <Helmet
-      title={data.site.siteMetadata.title}
-      meta={[
-        {
-          name: "description",
-          content:
-            "Compare Medicare payments and copays for certain procedures in hospital outpatient departments and ambulatory surgery centers."
-        },
-        {
-          name: "robots",
-          content: "noindex, nofollow"
-        }
-      ]}
-    />
-    {location.pathname !== "/" && (
-      <Header title={data.site.siteMetadata.title} />
-    )}
-    <main id="main">{children()}</main>
-  </div>
-);
+const TemplateWrapper = ({ children, data, location }) => {
+  const isHome = location.pathname === withPrefix("/");
+
+  return (
+    <div className="ds-base">
+      <SkipNav href="#main" />
+      <Helmet
+        title={data.site.siteMetadata.title}
+        meta={[
+          {
+            name: "description",
+            content:
+              "Compare Medicare payments and copays for certain procedures in hospital outpatient departments and ambulatory surgery centers."
+          },
+          { name: "robots", content: "noindex, nofollow" }
+        ]}
+      />
+      {!isHome && <Header title={data.site.siteMetadata.title} />}
+      <main id="main">{children()}</main>
+    </div>
+  );
+};
 
 TemplateWrapper.propTypes = {
   children: PropTypes.func,
