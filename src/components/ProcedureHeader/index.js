4ca622582c02c94f4fc40eb4f4bccc2c516290be
sawyerh <s.hollenshead@gmail.com>
2018-01-17 16:30:14 -0500

    Reduce number of <header>

--- src/components/ProcedureHeader/index.js
+++ src/components/ProcedureHeader/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 export default ({ procedure }) => (
-  <header className="ds-u-fill--gray-lightest ds-u-padding-y--7 ds-u-margin-bottom--4 ds-u-border-bottom--2">
+  <div className="ds-u-fill--gray-lightest ds-u-padding-y--7 ds-u-margin-bottom--4 ds-u-border-bottom--2">
     <div className="ds-l-container">
       <h1 className="ds-title">{procedure.name}</h1>
       <p className="ds-u-color--gray ds-u-margin-top--0">
         Procedure code: {procedure.code}
       </p>
     </div>
-  </header>
+  </div>
 );
