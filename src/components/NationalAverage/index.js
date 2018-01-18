748d65148984dab2cfac03f08c4f9e351ce3a8c0
sawyerh <s.hollenshead@gmail.com>
2018-01-18 10:13:42 -0500

    Tweak national avg. subheadings

--- src/components/NationalAverage/index.js
+++ src/components/NationalAverage/index.js
@@ -16,11 +16,13 @@ const Row = ({ children, price }) => (
 
 export default () => (
   <React.Fragment>
-    <h3 className="ds-h6">National average amount that Medicare pays</h3>
+    <h3 className="ds-u-font-size--small ds-u-color--primary-darker ds-u-margin-bottom--0">
+      National average amount that Medicare&nbsp;pays
+    </h3>
     <Row price="$1,250">At Hospital Outpatient Departments</Row>
     <Row price="$3,152">At Ambulatory Surgical Centers</Row>
 
-    <h3 className="ds-h6 ds-u-margin-top--4">
+    <h3 className="ds-u-font-size--small ds-u-color--primary-darker ds-u-margin-bottom--0 ds-u-margin-top--4">
       National average copay for a beneficiary with no Medicare supplemental
       insurance
     </h3>
