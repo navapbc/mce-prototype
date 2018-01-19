6a3e27ce9f51de067efe4fc48da0693cbdf65927
sawyerh <s.hollenshead@gmail.com>
2018-01-19 10:50:30 -0500

    Style /procedures

--- src/pages/procedures.js
+++ src/pages/procedures.js
@@ -1,18 +1,28 @@
 import Link from "gatsby-link";
 import React from "react";
+import _ from "lodash";
 
 const ProceduresPage = ({ data }) => {
+  let procedures = data.allProceduresJson.edges.map(o => o.node);
+  procedures = _.sortBy(procedures, "name");
+
   return (
-    <React.Fragment>
-      <h1>Procedures</h1>
-      <ul>
-        {data.allProceduresJson.edges.map(({ node }) => (
-          <li key={node.fields.slug}>
-            <Link to={`/${node.fields.slug}`}>{node.name}</Link>
+    <div className="ds-l-container ds-u-margin-top--7">
+      <h1 className="ds-h1">Procedures</h1>
+      <ul className="ds-c-list ds-c-list--bare">
+        {procedures.map(p => (
+          <li
+            key={p.fields.slug}
+            className="ds-u-margin-bottom--2 ds-u-border-top--1 ds-u-padding-top--2"
+          >
+            <h2 className="ds-h4 ds-u-margin-bottom--0">
+              <Link to={p.fields.slug}>{p.name}</Link>
+            </h2>
+            <span className="ds-u-color--muted">Code: {p.code}</span>
           </li>
         ))}
       </ul>
-    </React.Fragment>
+    </div>
   );
 };
 
@@ -27,6 +37,7 @@ export const query = graphql`
             slug
           }
           name
+          code
         }
       }
     }
