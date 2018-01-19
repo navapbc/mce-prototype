fc5f48c4fe97b56a22ac0590a5a8b285faaafb02
sawyerh <s.hollenshead@gmail.com>
2018-01-19 15:26:20 -0500

    Fix flash of homepage content

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,11 +3,17 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import PropTypes from "prop-types";
 import React from "react";
-import { SkipNav } from "@cmsgov/design-system-core";
+import SkipNav from "@cmsgov/design-system-core/dist/components/SkipNav/SkipNav";
 import { withPrefix } from "gatsby-link";
 
 const TemplateWrapper = ({ children, data, location }) => {
-  const isHome = location.pathname === withPrefix("/");
+  // This double check is a Gatsby workaround. It seems when it builds
+  // the static HTML page, it's not using the prefix. If we no longer
+  // set the pathPrefix property in gatsby-config.js, then we can remove
+  // the withPrefix("/") check
+  const isHome =
+    location.pathname === withPrefix("/") || location.pathname === "/";
+
   return (
     <div className="ds-base">
       <SkipNav href="#main" />
